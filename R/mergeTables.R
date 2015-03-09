b3f81edc192c793de3faf75d5289ba87d93ae81f
Christine Laney <claney@neoninc.org>
2015-03-09 13:50:15 -0600

    Updated contents of table per Sarah & Katie comments

--- R/mergeTables.R
+++ R/mergeTables.R
@@ -9,15 +9,18 @@
 #' 
 mergeTables <- function(plantsamples,phenoobs,anntable){
   df <- merge(plantsamples, phenoobs, by = "Tag ID", all = TRUE)
+  countlevels <- 
   df$"Tag ID"  <- substr(df$"Tag ID", 19,24)
-  df$"From Transect" <- paste0(substr(df$"Direction From Transect", 1, 1), df$"Ninety Degree Distance")
+  df$"From Transect" <- paste0(substr(df$"Direction From Transect", 1, 1), 
+                               df$"Ninety Degree Distance")
   df$"Phenophase Status" <- substr(df$"Phenophase Status", 1, 1)
-  df$"Phenophase Intensity"[(df$"Phenophase Intensity" %in% c("< 3","3-10","11-100","101-1,000","1,001-10,000","> 10,000"))] <- "\\#"
-  df$"Phenophase Intensity"[!(df$"Phenophase Intensity" %in% c("< 3","3-10","11-100","101-1,000","1,001-10,000","> 10,000"))] <- "\\%"
+  df$"Phenophase Intensity"[(df$"Phenophase Intensity" %in% countlevels)] <- "\\#"
+  df$"Phenophase Intensity"[!(df$"Phenophase Intensity" %in% countlevels)] <- "\\%"
   df$"Remarks" <- character(nrow(df))
   rownames(df, do.NULL = TRUE)
-  #df <- df[,c(5,1:3,7:9,6,10)]
-  #names(df) <- c("Spp. Code","Tag ID","Trans. Meter","Dir. from Trans.",
-  #               "Phenophase Desc.","Pheno. Status (y/n/m/?)","Pheno. Intensity","Drop","Remarks")
+  df <- df[,c(5,1:2,10,7:8,6,11)]
+  names(df) <- c("Taxon ID","Tag ID", "Trans. Meter","From Trans.",
+                 "Phenophase Desc.","Pheno. Status (y/n/m/?)",
+                 "Pheno. Intensity","Drop","Remarks")
   return(df)
 }
