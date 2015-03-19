0dd80c835841bd3e60512ef69b1bc4964d55970d
Christine Laney <claney@neoninc.org>
2015-03-19 12:57:48 -0600

    combine previous status and intensity columns, clarify code

--- R/mergeTables.R
+++ R/mergeTables.R
@@ -11,21 +11,28 @@ mergeTables <- function(plantsamples,phenoobs){
   df <- merge(plantsamples, phenoobs, by = "Tag ID", all = TRUE)
   df <- df[which(df$"Drop Plant"=="active"),]
   df$"Drop Plant" <- rep("", nrow(df))
-  countlevels <- c("< 3","3-10","11-100","101-1,000","1,001-10,000","> 10,000")
   df$"Tag ID"  <- substr(df$"Tag ID", 19,24)
   df$"From Transect" <- paste0(substr(df$"Direction From Transect", 1, 1), 
                                df$"Ninety Degree Distance")
   df$"Phenophase Status" <- substr(df$"Phenophase Status", 1, 1)
-  df$"Prev. Pheno. Intensity" <- character(nrow(df))
-  df$"Prev. Pheno. Intensity"[(df$"Phenophase Intensity" %in% countlevels)] <- 
-    paste("\\#", df$"Phenophase Intensity"[(df$"Phenophase Intensity" %in% countlevels)])
-  df$"Prev. Pheno. Intensity"[!(df$"Phenophase Intensity" %in% countlevels)] <- 
-    paste("\\%", df$"Phenophase Intensity"[!(df$"Phenophase Intensity" %in% countlevels)]) 
+  
+  countlevels <- c("< 3","3-10","11-100","101-1,000","1,001-10,000","> 10,000")
+  numbers <- df$"Phenophase Intensity"[which(df$"Phenophase Intensity" %in% countlevels)]
+  percents <- df$"Phenophase Intensity"[which(!df$"Phenophase Intensity" %in% countlevels)]               
+  
+  df$"Prev. Pheno. Status & Intensity" <- character(nrow(df))
+  df$"Prev. Pheno. Status & Intensity"[numbers] <- paste(
+    df$"Phenophase Status"[numbers], " ", df$"Phenophase Intensity"[numbers], 
+    "\\%", sep = "")
+  df$"Prev. Pheno. Status & Intensity"[percents] <- paste(
+    df$"Phenophase Status"[percents], " ", df$"Phenophase Intensity"[percents], 
+    "\\#", sep = "")  
+  
   df$"Remarks" <- character(nrow(df))
   rownames(df, do.NULL = TRUE)
-  df <- df[,c(5,1:2,10,7:8,11,9,6,12)]
+  df <- df[,c(5,1:2,9,7,10,8,6,11)]
   names(df) <- c("Taxon ID","Tag ID", "Trans. Meter","From Trans.",
                  "Phenophase Desc.","Pheno. Status y/n/m/?",
-                 "Past Pheno. Intensity","Pheno. Intensity","Drop","Remarks")
+                 "Past Pheno. Status & Intensity","Pheno. Intensity","Drop","Remarks")
   return(df)
 }
